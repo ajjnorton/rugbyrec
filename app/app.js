870916535b6c5bfa5f7bae8a271e10337c9320f9
ajjnorton <anthony@modularity.works>
2016-03-19 11:46:05 +0300

    added file upload

--- app/app.js
+++ app/app.js
@@ -12,6 +12,7 @@
 
     ])
 
+
     app.config(function($stateProvider, $urlRouterProvider) {
 
         // any unm  atched url, redirect to
@@ -30,11 +31,57 @@
             })
             .state('add_image', {
                 url: '/add_image',
-                templateUrl: 'modules/add_image/view/add_image.html',
-                controller: 'add_imageCtrl'
+                templateUrl: 'modules/add_image/view/file_upload.html',
+                controller: 'UploadCtrl'
             })
 
 
     })
+
+    app.directive('ngThumb', ['$window', function($window) {
+        var helper = {
+            support: !!($window.FileReader && $window.CanvasRenderingContext2D),
+            isFile: function(item) {
+                return angular.isObject(item) && item instanceof $window.File;
+            },
+            isImage: function(file) {
+                var type = '|' + file.type.slice(file.type.lastIndexOf('/') + 1) + '|';
+                return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+            }
+        };
+
+        return {
+            restrict: 'A',
+            template: '<canvas/>',
+            link: function(scope, element, attributes) {
+                if (!helper.support) return;
+
+                var params = scope.$eval(attributes.ngThumb);
+
+                if (!helper.isFile(params.file)) return;
+                if (!helper.isImage(params.file)) return;
+
+                var canvas = element.find('canvas');
+                var reader = new FileReader();
+
+                reader.onload = onLoadFile;
+                reader.readAsDataURL(params.file);
+
+                function onLoadFile(event) {
+                    var img = new Image();
+                    img.onload = onLoadImage;
+                    img.src = event.target.result;
+                }
+
+                function onLoadImage() {
+                    var width = params.width || this.width / this.height * params.height;
+                    var height = params.height || this.height / this.width * params.width;
+                    canvas.attr({ width: width, height: height });
+                    canvas[0].getContext('2d').drawImage(this, 0, 0, width, height);
+                }
+            }
+        };
+    }]);
+
 
 })();
