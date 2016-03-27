b56cf05168f034d7d2ddf0b3535ce2a331b62653
ajjnorton <anthony@modularity.works>
2016-03-27 18:41:46 +0300

    end of day

--- app/modules/add_image/uploadCtrl.js
+++ app/modules/add_image/uploadCtrl.js
@@ -5,6 +5,8 @@ var ctrl = angular.module('add_image.controllers', [])
  */
 ctrl.controller('UploadCtrl', ['$scope', 'FileUploader', '$firebaseArray', 'toaster',
     function($scope, FileUploader, $firebaseArray, toaster) {
+
+        // clear all existing toast messages when page launches
         toaster.clear();
 
         $scope.pop = function() {
@@ -14,23 +16,23 @@ ctrl.controller('UploadCtrl', ['$scope', 'FileUploader', '$firebaseArray', 'toas
         var ref = new Firebase('https://rugbyrec-app.firebaseio.com');
         var list = $firebaseArray(ref);
 
-       
 
-      
-            
-        //toaster.pop('wait', "Please wait ..", "Loading data");
+
+
+
+        // launch toaster message
         toaster.pop('wait', "Please wait", '<h6 style="color:#fff;">Loading data</h6>', -1, 'trustedHtml');
-    
 
 
+        // load list of players and matched for the typeahead
         list.$loaded()
             .then(function(data) {
                 $scope.rugbyPhotos = data;
-                console.log($scope.rugbyPhotos);
                 $scope.loaded = true;
-               
-               toaster.clear();
-               $scope.pop();
+                // clear toast messages
+                toaster.clear();
+                // launch confirmation toaster message
+                $scope.pop();
 
             })
             .catch(function(error) {
@@ -40,13 +42,14 @@ ctrl.controller('UploadCtrl', ['$scope', 'FileUploader', '$firebaseArray', 'toas
 
 
 
-
+        // Point to the PHP file that will manage the upload
         var uploaderImages = $scope.uploaderImages = new FileUploader({
-            url: 'upload.php'
+            url: 'php/image_upload1.php'
         });
 
-        // FILTERS
 
+
+        // FILTERS
         uploaderImages.filters.push({
             name: 'imageFilter',
             fn: function(item /*{File|FileLikeObject}*/ , options) {
@@ -58,93 +61,40 @@ ctrl.controller('UploadCtrl', ['$scope', 'FileUploader', '$firebaseArray', 'toas
         // CALLBACKS
 
         uploaderImages.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/ , filter, options) {
-            console.info('onWhenAddingFileFailed', item, filter, options);
+            //console.info('onWhenAddingFileFailed', item, filter, options);
         };
         uploaderImages.onAfterAddingFile = function(fileItem) {
-            console.info('onAfterAddingFile', fileItem);
+            //console.info('onAfterAddingFile', fileItem);
         };
         uploaderImages.onAfterAddingAll = function(addedFileItems) {
-            console.info('onAfterAddingAll', addedFileItems);
+            //console.info('onAfterAddingAll', addedFileItems);
         };
         uploaderImages.onBeforeUploadItem = function(item) {
-            console.info('onBeforeUploadItem', item);
+            //console.info('onBeforeUploadItem', item);
         };
         uploaderImages.onProgressItem = function(fileItem, progress) {
-            console.info('onProgressItem', fileItem, progress);
+            //console.info('onProgressItem', fileItem, progress);
         };
         uploaderImages.onProgressAll = function(progress) {
-            console.info('onProgressAll', progress);
+            //console.info('onProgressAll', progress);
         };
         uploaderImages.onSuccessItem = function(fileItem, response, status, headers) {
             console.info('onSuccessItem', fileItem, response, status, headers);
         };
         uploaderImages.onErrorItem = function(fileItem, response, status, headers) {
-            console.info('onErrorItem', fileItem, response, status, headers);
+            //console.info('onErrorItem', fileItem, response, status, headers);
         };
         uploaderImages.onCancelItem = function(fileItem, response, status, headers) {
-            console.info('onCancelItem', fileItem, response, status, headers);
+            //console.info('onCancelItem', fileItem, response, status, headers);
         };
         uploaderImages.onCompleteItem = function(fileItem, response, status, headers) {
-            console.info('onCompleteItem', fileItem, response, status, headers);
+            //console.info('onCompleteItem', fileItem, response, status, headers);
         };
         uploaderImages.onCompleteAll = function() {
-            console.info('onCompleteAll');
+            //console.info('onCompleteAll');
         };
 
         console.info('uploader', uploaderImages);
     }
 ]);
-ctrl.controller('UploadCtrl2', ['$scope', 'FileUploader',
-    function($scope, FileUploader) {
-        var uploader = $scope.uploader = new FileUploader({
-            url: 'upload.php'
-        });
 
-        // FILTERS
-
-        uploader.filters.push({
-            name: 'customFilter',
-            fn: function(item /*{File|FileLikeObject}*/ , options) {
-                return this.queue.length < 10;
-            }
-        });
-
-        // CALLBACKS
-
-        uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/ , filter, options) {
-            console.info('onWhenAddingFileFailed', item, filter, options);
-        };
-        uploader.onAfterAddingFile = function(fileItem) {
-            console.info('onAfterAddingFile', fileItem);
-        };
-        uploader.onAfterAddingAll = function(addedFileItems) {
-            console.info('onAfterAddingAll', addedFileItems);
-        };
-        uploader.onBeforeUploadItem = function(item) {
-            console.info('onBeforeUploadItem', item);
-        };
-        uploader.onProgressItem = function(fileItem, progress) {
-            console.info('onProgressItem', fileItem, progress);
-        };
-        uploader.onProgressAll = function(progress) {
-            console.info('onProgressAll', progress);
-        };
-        uploader.onSuccessItem = function(fileItem, response, status, headers) {
-            console.info('onSuccessItem', fileItem, response, status, headers);
-        };
-        uploader.onErrorItem = function(fileItem, response, status, headers) {
-            console.info('onErrorItem', fileItem, response, status, headers);
-        };
-        uploader.onCancelItem = function(fileItem, response, status, headers) {
-            console.info('onCancelItem', fileItem, response, status, headers);
-        };
-        uploader.onCompleteItem = function(fileItem, response, status, headers) {
-            console.info('onCompleteItem', fileItem, response, status, headers);
-        };
-        uploader.onCompleteAll = function() {
-            console.info('onCompleteAll');
-        };
-
-        console.info('uploader', uploader);
-    }
-]);
